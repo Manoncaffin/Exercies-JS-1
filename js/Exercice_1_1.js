8ad095b577a40e578d819b0ab75cac8e90733204
Caffin <caffinmanon@gmail.com>
2023-11-23 15:29:34 +0100

    Exercice 1

---
+++ js/Exercice_1_1.js
@@ -0,0 +1,3 @@
+let nom = 'Caffin';
+alert('Caffin');
+console(log(alert))
---
+++ js/Exercice_1_2.js
@@ -0,0 +1,9 @@
+let Nom = 'Doe';
+
+let Prenom = 'John';
+
+let Ville = 'New-York';
+
+let message = 'Nom :' + 'Prenom :' + 'Ville'; 
+
+console.log (Nom + Prenom + Ville)
