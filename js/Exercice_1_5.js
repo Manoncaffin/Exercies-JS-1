9878ba8e70b037b130b9982e9323488631ba09b4
Caffin <caffinmanon@gmail.com>
2023-11-24 10:22:02 +0100

    Exercice 7

--- js/Exercice_1_5.js
+++ js/Exercice_1_5.js
@@ -1,2 +1,16 @@
 let nombre1 = prompt("entrez un premier nombre");
 let nombre2 = prompt("entrez un second nombre");
+
+function multiplication (a, b){
+    return (a * b)
+}
+
+console.log (multiplication (2.10, 3.30));
+
+
+
+let nombre1 = Math.trunc(2.10);
+let nombre2 = Math.trunc(3.20);
+
+console.log('Math.trunc'(6.93));
+
